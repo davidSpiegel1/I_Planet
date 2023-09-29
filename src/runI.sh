bd6efeb1aafa606bf3a0ad6654cb274f96bf1cbf
kyle <stonewal7@outlook.com>
2023-09-28 17:03:27 -0700

    Changed hard path to user directory to home variable

--- src/runI.sh
+++ src/runI.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-javac --module-path /Users/davidspiegel/Downloads/Important_Insta/javafx-sdk-17.0.1/lib --add-modules javafx.controls,javafx.fxml view/IplanetFX.java
+javac --module-path $HOME/Downloads/Important_Insta/javafx-sdk-17.0.1/lib --add-modules javafx.controls,javafx.fxml view/IplanetFX.java
 
-java --module-path /Users/davidspiegel/Downloads/Important_Insta/javafx-sdk-17.0.1/lib --add-modules javafx.controls,javafx.fxml view.IplanetFX
+java --module-path $HOME/Downloads/Important_Insta/javafx-sdk-17.0.1/lib --add-modules javafx.controls,javafx.fxml view.IplanetFX
